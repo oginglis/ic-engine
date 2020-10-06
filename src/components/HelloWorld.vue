ec2bfe0efcd9f0be7a3223949179ec3d7a568422
Oliver Inglis <ollie@theinglistwins.com>
2020-10-06 22:12:44 +0100

    attempting to fix gltf import error

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,7 +3,8 @@
 </template>
 
 <script>
-import * as Three from 'three'
+import * as Three from 'three';
+import { GLTFLoader } from 'three/examples/jsm/loaders/GLTFLoader.js';
 
 export default {
   name: 'ThreeTest',
@@ -12,28 +13,52 @@ export default {
       camera: null,
       scene: null,
       renderer: null,
-      mesh: null
+      mesh: null,
+      model: null,
     }
   },
   methods: {
     init: function() {
-        let container = document.getElementById('container');
+      let container = document.getElementById('container');
 
-        this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
-        this.camera.position.z = 1;
+      this.camera = new Three.PerspectiveCamera(70, container.clientWidth/container.clientHeight, 0.01, 10);
+      this.camera.position.z = 1;
 
-        this.scene = new Three.Scene();
+      this.scene = new Three.Scene();
 
-        let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
-        let material = new Three.MeshNormalMaterial();
+      let geometry = new Three.BoxGeometry(0.2, 0.2, 0.2);
+      let material = new Three.MeshNormalMaterial();
 
-        this.mesh = new Three.Mesh(geometry, material);
-        this.scene.add(this.mesh);
+      this.mesh = new Three.Mesh(geometry, material);
+      this.scene.add(this.mesh);
 
-        this.renderer = new Three.WebGLRenderer({antialias: true});
-        this.renderer.setSize(container.clientWidth, container.clientHeight);
-        container.appendChild(this.renderer.domElement);
+      this.renderer = new Three.WebGLRenderer({antialias: true});
+      this.renderer.setSize(container.clientWidth, container.clientHeight);
+      container.appendChild(this.renderer.domElement);
 
+      // Instantiate a loader
+      var loader = new GLTFLoader();
+
+      // Load a glTF resource
+      loader.load(
+        // resource URL
+        '../assets/modeltest.gltf',
+        // called when the resource is loaded
+        function ( gltf ) {		
+          console.log(gltf);   
+          this.scene.add( gltf.scene);
+        },
+        // called while loading is progressing
+        function ( xhr ) {
+          console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+        },
+        // called when loading has errors
+        function ( error ) {
+          console.log(error)
+          console.log( 'An error happened ' + error );
+          
+        }
+      );
     },
     animate: function() {
         requestAnimationFrame(this.animate);
